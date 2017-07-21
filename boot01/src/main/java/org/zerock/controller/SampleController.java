8cef61cb0b4656e464c63ca7491dd483dae29df3
ahnboks <ahnboks@naver.com>
2017-07-21 17:42:29 +0900

    boot01 실습 예제(@Test, Lombok, 간단 어노테이션)

---
+++ boot01/src/main/java/org/zerock/controller/SampleController.java
@@ -0,0 +1,26 @@
+package org.zerock.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.zerock.domain.SampleVO;
+
+@RestController
+public class SampleController {
+	@GetMapping("/hello")
+	public String sayHello() {
+		return "Hello World";
+	}
+	
+	@GetMapping("/sample")
+	public SampleVO makeSampleVO() {
+		SampleVO vo = new SampleVO();
+		vo.setVal1("v1");
+		vo.setVal2("v2");
+		vo.setVal3("v3");
+		
+		System.out.println(vo);
+		
+		return vo;
+	}
+
+}
