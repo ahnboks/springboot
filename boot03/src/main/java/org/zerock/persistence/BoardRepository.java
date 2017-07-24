ab68015e64f9a76ddfae80c2b561967cc760c794
ahnboks <ahnboks@naver.com>
2017-07-24 15:42:47 +0900

    boot03 실습 예제(쿼리메소드, querydsl, 페이징 처리)

--- boot03/src/main/java/org/zerock/persistence/BoardRepository.java
+++ boot03/src/main/java/org/zerock/persistence/BoardRepository.java
@@ -1,11 +1,53 @@
 package org.zerock.persistence;
 
+
+import java.util.Collection;
 import java.util.List;
 
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.querydsl.QueryDslPredicateExecutor;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.zerock.domain.Board;
 
 
-public interface BoardRepository extends CrudRepository<Board, Long> {
+public interface BoardRepository extends CrudRepository<Board, Long>,QueryDslPredicateExecutor<Board> {
 	public List<Board> findBoardByTitle(String title);
+	
+	//public Collection<Board> findByWriter(String writer);
+	
+	public Collection<Board> findByWriterContaining(String writer);
+	
+	public Collection<Board> findByTitleContainingOrContentContaining(String title, String content);
+	
+	public Collection<Board> findByTitleContainingAndBnoGreaterThan(String keyword,Long num);
+	
+	public Collection<Board> findByBnoGreaterThanOrderByBnoDesc(Long bno);
+	
+	public List<Board> findByBnoGreaterThanOrderByBnoDesc(Long bno, Pageable paging);
+	
+	//public List<Board> findByBnoGreaterThan(Long bno,Pageable paging);
+	
+	public Page<Board> findByBnoGreaterThan(Long bno,Pageable paging);
+	
+//	@Query("select b from Board b where b.title like %?1% and b.bno>0 order by b.bno desc")
+//	public List<Board> findByTitle(String title);
+	
+	@Query("select b from Board b where b.title like %:content% and b.bno>0 order by b.bno desc")
+	public List<Board> findByContent(@Param("content") String content);
+	
+	@Query("select b from #{#entityName} b where b.title like %?1% and b.bno>0 order by b.bno desc")
+	public List<Board> findByWriter(String writer);
+	
+//	@Query("select b.bno, b.title, b.writer, b.regdate from Board b where b.title like %?1% and b.bno>0 order by b.bno desc")
+//	public List<Object[]> findByTitle(String title);
+	
+	@Query(value="select bno, title, writer from tbl_boards where title like concat('%', ?1, '%') and bno>0 order by bno desc", nativeQuery=true)
+	public List<Object[]> findByTitle(String title);
+	
+	@Query("select b from Board b where b.bno > 0 order by b.bno desc")
+	public List<Board> findByPage(Pageable paging);
 }
--- boot03/src/test/java/org/zerock/Boot03ApplicationTests.java
+++ boot03/src/test/java/org/zerock/Boot03ApplicationTests.java
@@ -1,15 +1,27 @@
 package org.zerock;
 
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.zerock.domain.Board;
+import org.zerock.domain.QBoard;
 import org.zerock.persistence.BoardRepository;
 
+
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.QBean;
+
+@SuppressWarnings("unused")
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class Boot03ApplicationTests {
@@ -27,11 +39,131 @@ public class Boot03ApplicationTests {
 			repo.save(board);
 		}
 	}
-	
+	/*
 	@Test
 	public void testByTitle() {
 		repo.findBoardByTitle("제목..177")
 		.forEach(board->System.out.println(board));
 	}
+	
+	@Test
+	public void testByWriter() {
+		Collection<Board> results = repo.findByWriter("user00");
+		results.forEach(board->System.out.println(board));
+	}
+	*/
+	@Test
+	public void testByWriterContaining() {
+		Collection<Board> results = repo.findByWriterContaining("05");
+		results.forEach(board->System.out.println(board));
+	}
+	
+	@Test
+	public void testByTitleAndBno() {
+		Collection<Board> results = repo.findByTitleContainingAndBnoGreaterThan("5", 50L);
+		results.forEach(board->System.out.println(board));
+	}
+	
+	@Test
+	public void testBnoGreaterThanOrderByBnoDesc() {
+		Collection<Board> results = repo.findByBnoGreaterThanOrderByBnoDesc(90L);
+		results.forEach(board->System.out.println(board));
+	}
+	
+	@Test
+	public void testBnoOrderByPaging() {
+		
+		Pageable paging = new PageRequest(0,10);
+		Collection<Board> results = repo.findByBnoGreaterThanOrderByBnoDesc(0L, paging);
+		results.forEach(board->System.out.println(board));
+	}
+	
+	/*
+	@Test
+	public void testBnoPagingSort1() {
+
+		Pageable paging = new PageRequest(0, 10, Sort.Direction.DESC, "bno");
+		Collection<Board> results = repo.findByBnoGreaterThan(100L, paging);
+		results.forEach(b -> System.out.println(b));
+
+	}
+	*/
+	
+
+	@Test
+	public void testBnoPagingSort() {
+
+		Pageable paging = new PageRequest(0, 10, Sort.Direction.ASC, "bno");
+		Page<Board> results = repo.findByBnoGreaterThan(100L, paging);
+		System.out.println("Page Size : " + results.getSize());
+
+		System.out.println("Total Pages : " + results.getTotalPages());
+
+		System.out.println("Total Count : " + results.getTotalElements());
+
+		System.out.println("Next : " + results.nextPageable());
+
+		results.forEach(board -> System.out.println(board));
+
+	}
+	
+	/*@Test
+	public void testByTitle() {
+		repo.findByTitle("17").forEach(board->System.out.println(board));
+	}*/
+	
+	@Test
+	public void testByContent() {
+		repo.findByContent("30").forEach(board->System.out.println(board));
+	}
+	
+	@Test
+	public void testByWriter() {
+		repo.findByWriter("00").forEach(board->System.out.println(board));
+	}
+	
+	@Test
+	public void testByTitle() {
+		repo.findByTitle("17").forEach(arr->System.out.println(Arrays.toString(arr)));
+	}
+	
+	@Test
+	public void testByPaging() {
+		Pageable pageable = new PageRequest(0, 10);
+		repo.findByPage(pageable).forEach(board->System.out.println(board));
+	}
+	
+	@Test
+	public void testPredicate() {
+		String type="t";
+		String keyword="17";
+		
+		BooleanBuilder builder = new BooleanBuilder();
+		
+		QBoard board =QBoard.board;
+		
+		if(type.equals("t")) {
+			builder.and(board.title.like("%"+keyword+"%"));
+		}
+		
+		builder.and(board.bno.gt(0L));
+		
+		Pageable pageable = new PageRequest(0, 10);
+		
+		Page<Board> result=repo.findAll(builder, pageable);
+		
+		System.out.println("Page Size : " + result.getSize());
+
+		System.out.println("Total Pages : " + result.getTotalPages());
+
+		System.out.println("Total Count : " + result.getTotalElements());
+
+		System.out.println("Next : " + result.nextPageable());
+		
+		List<Board> list=result.getContent();
+		list.forEach(b->System.out.println(b));
+
+	}
+
 
 }
